f9a8ddace63d3bf167caf9b395097c5132504707
= <paramsavjani3010@gmail.com>
2025-03-02 18:11:24 +0530

    feat: update User model to include findOrCreateByPublicKey method and enforce required fields and added the dummy matches

--- src/lib/model/User.ts
+++ src/lib/model/User.ts
@@ -1,30 +1,40 @@
-import { Schema, model, models, Document } from "mongoose";
+import { Schema, model, models, type Document } from "mongoose";
 
 interface IUser extends Document {
   email: string;
   name?: string;
-  profilePicture?: string;
   provider: string;
   createdAt: Date;
   updatedAt: Date;
-  balance: number;
   publicKey: string;
-  privateKey: string;
 }
 
 export type { IUser };
+
 const UserSchema = new Schema<IUser>(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String },
-    profilePicture: { type: String },
     provider: { type: String, required: true },
-    balance: { type: Number, default: 100 },
-    publicKey: { type: String },
-    privateKey: { type: String },
+    publicKey: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
+
+// Static method to findOrCreate a user by publicKey
+UserSchema.statics.findOrCreateByPublicKey = async function (
+  publicKey: string
+) {
+  let user = await this.findOne({ publicKey });
+  if (!user) {
+    user = await this.create({
+      email: `${publicKey}@placeholder.com`, // Placeholder email
+      provider: "wallet",
+      publicKey,
+    });
+  }
+  return user;
+};
 
 const User = models.User || model<IUser>("User", UserSchema);
 export default User;
